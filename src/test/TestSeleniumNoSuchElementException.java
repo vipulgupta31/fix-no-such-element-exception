653aa35a210f018e4373e56cb5165856d09048a9
Vipul Gupta <71564722+vipulgupta31@users.noreply.github.com>
2023-04-11 22:32:40 +0530

    Update TestSeleniumNoSuchElementException.java

--- src/test/TestSeleniumNoSuchElementException.java
+++ src/test/TestSeleniumNoSuchElementException.java
@@ -11,77 +11,85 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.*;
 
-
-
 public class TestSeleniumNoSuchElementException {
 
 	public RemoteWebDriver driver = null;
 
+	String username = System.getenv("LT_USERNAME") == null ? "<lambdatest_username>" : System.getenv("LT_USERNAME");
+	String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "<lambdatest_accesskey>"
+			: System.getenv("LT_ACCESS_KEY");
 
-	   String username = System.getenv("LT_USERNAME") == null ? "<lambdatest_username>" : System.getenv("LT_USERNAME");
-	   String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "<lambdatest_accesskey>" : System.getenv("LT_ACCESS_KEY");
+	@BeforeTest
+	public void setup() {
+		try {
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.setPlatformName("Windows 10");
+			chromeOptions.setBrowserVersion("110.0");
 
+			HashMap<String, Object> ltOptions = new HashMap<String, Object>();
+			ltOptions.put("build", "NoSuchElementException in Selenium");
+			ltOptions.put("name", "NoSuchElementException Fix with WebDriverWait");
+			chromeOptions.setCapability("LT:Options", ltOptions);
 
-	   @BeforeTest
-	   public void setup() {
-	       try {
-	           ChromeOptions chromeOptions = new ChromeOptions();
-	           chromeOptions.setPlatformName("Windows 10");
-	           chromeOptions.setBrowserVersion("110.0");
+			driver = new RemoteWebDriver(
+					new URL("https://" + username + ":" + accessKey + "@hub.lambdatest.com/wd/hub"), chromeOptions);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 
+	}
 
-	           HashMap<String, Object> ltOptions = new HashMap<String, Object>();
-	           ltOptions.put("build", "NoSuchElementException in Selenium");
-	           chromeOptions.setCapability("LT:Options", ltOptions);
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+	}
 
+	@Test
+	public void testNoSuchElmentException() {
+		driver.get("https://ecommerce-playground.lambdatest.io/");
+		driver.findElement(By.id("entry_213244")).click();
+	}
 
-	           driver = new RemoteWebDriver(
-	                   new URL("https://" + username + ":" + accessKey + "@hub.lambdatest.com/wd/hub"), chromeOptions);
-	       } catch (MalformedURLException e) {
-	           e.printStackTrace();
-	       }
-	       driver.get("https://www.lambdatest.com/selenium-playground/");
-	   }
-	   
-	   @AfterTest
-	   public void tearDown() {
-	       driver.quit();
-	   }
+	@Test
+	public void testNoSuchElmentException_fix_tryCatch() {
+		driver.get("https://ecommerce-playground.lambdatest.io/");
+		try {
+			driver.findElement(By.id("entry_213244")).click();
+		} catch (NoSuchElementException ex) {
+			System.out.println("We are inside catch block");
+			System.out.println("NoSuchElmentException has been handled.");
+		}
+	}
 
-	   @Test
-	   public void testNoSuchElmentException()
-	   {
-		   	driver.findElement(By.xpath("//*[contains(text(),'Table Pagination')]")).click();
-		   	driver.findElement(By.id("maxRows")).click();
-	   }
-	   
-	   @Test
-	   public void testNoSuchElmentException_fix_tryCatch()
-	   {
-		   try {
-			   driver.findElement(By.xpath("//*[contains(text(),'Table Pagination')]")).click();
-			   driver.findElement(By.id("maxRows")).click();   
-		   }
-		   catch(NoSuchElementException ex)
-		   {
-			   System.out.println("We are inside catch block");
-			   System.out.println("NoSuchElmentException has been handled.");
-		   }
-	   }
-	   
-	   @Test
-	   public void testNoSuchElmentException_fix_wait()
-	   {
-		   	driver.findElement(By.xpath("//*[contains(text(),'Table Pagination')]")).click();
-		   	
-		   	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-		    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("maxRows")));
-		   	
-		    driver.findElement(By.id("maxRows")).click();
-			System.out.println("Web element located and clicked successfully.");
+	@Test
+	public void testNoSuchElmentException_fix_wait() {
+		driver.get("https://ecommerce-playground.lambdatest.io/");
 
-	   }
-	   
-	   
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("entry_213244")));
+
+		driver.findElement(By.id("entry_213244")).click();
+		System.out.println("Web element located and clicked successfully.");
+	}
+
+	@Test
+	public void testNoSuchElmentException_iframe() {
+		driver.get("https://play1.automationcamp.ir/frames.html");
+		driver.findElement(By.id("click_me_1")).click();
+	}
+
+	@Test
+	public void testNoSuchElmentException_iframe_fix() {
+		driver.get("https://play1.automationcamp.ir/frames.html");
+
+		driver.switchTo().frame("frame1");
+		System.out.println("Switched to frame 1");
+
+		driver.findElement(By.id("click_me_1")).click();
+		System.out.println("Web element located and clicked successfully.");
+
+		driver.switchTo().defaultContent();
+		System.out.println("Switched back to main window");
+	}
 
 }
